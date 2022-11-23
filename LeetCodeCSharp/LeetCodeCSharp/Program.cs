8d3d59fac21fdf1839872d0fc9d4297d8f2b9486
Aki <71226118+Aki-Gonzalez@users.noreply.github.com>
2022-11-23 12:39:23 +0100

    Remove Nth Node From end of List.

--- LeetCodeCSharp/LeetCodeCSharp/Program.cs
+++ LeetCodeCSharp/LeetCodeCSharp/Program.cs
@@ -1,5 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-LeetCodeCSharp.ValidParentheses.IsValid("{[]}");
+using LeetCodeCSharp;
+RemoveNthNodeFromEndofList.ListNode node5 = new RemoveNthNodeFromEndofList.ListNode(5, null);
+RemoveNthNodeFromEndofList.ListNode node4 = new RemoveNthNodeFromEndofList.ListNode(4, node5);
+RemoveNthNodeFromEndofList.ListNode node3 = new RemoveNthNodeFromEndofList.ListNode(3, node4);
+RemoveNthNodeFromEndofList.ListNode node2 = new RemoveNthNodeFromEndofList.ListNode(2, node3);
+RemoveNthNodeFromEndofList.ListNode node1 = new RemoveNthNodeFromEndofList.ListNode(1, node2);
 
-Console.WriteLine("Hello, World!");
+void PrintNodeList(RemoveNthNodeFromEndofList.ListNode head)
+{
+    RemoveNthNodeFromEndofList.ListNode node = head;
+    do
+    {
+        Console.Write(node.val.ToString() + ", ");
+        node = node.next;
+    } while (node != null);
+    Console.WriteLine();
+}
+
+PrintNodeList(node1);
+RemoveNthNodeFromEndofList.RemoveNthFromEnd(node1, 2);
+PrintNodeList(node1);
+RemoveNthNodeFromEndofList.RemoveNthFromEnd(node1, 4);
+PrintNodeList(node1);
+
+
---
+++ LeetCodeCSharp/LeetCodeCSharp/RemoveNthNodeFromEndofList.cs
@@ -0,0 +1,109 @@
+﻿
+
+namespace LeetCodeCSharp
+{
+    class RemoveNthNodeFromEndofList
+    {
+ 
+        public class ListNode {
+            public int val;
+            public ListNode next;
+            public ListNode(int val=0, ListNode next=null) {
+                this.val = val;
+                this.next = next;
+            }
+        }
+ 
+        static public ListNode RemoveNthFromEnd(ListNode head, int n)
+        {
+            /*
+                We start from the first node.
+                We want to remove the node on length - n, what is length?
+                Could iterate through to figure out the length
+                    ListNode node = head
+                    length = 1
+                    while node.next != null
+                        length++
+                        node = node.next
+
+                    toRemove = length - n
+                    current = 0
+                    node = head
+                    // 1, 2, 3, 4, 5, 6 
+                    while(current < toRemove)
+                        node = node.next
+                        current++
+                    
+                    node.next = node.next.next
+                    
+                    return head    
+                        
+             */
+
+            // Follow up: Could you do this in one pass?
+            // Second solution: single while-loop and a list, shorter runtime, more memory
+            if (head == null)
+                return head;
+
+            List<ListNode> nodeList = new List<ListNode>();
+            nodeList.Add(head);
+            while (nodeList[nodeList.Count - 1].next != null)
+            {
+                nodeList.Add(nodeList[nodeList.Count - 1].next);
+            }
+
+            int toRemove = nodeList.Count - n;
+            if (toRemove == 0)
+            {
+                head = head.next;
+                return head;
+            }
+
+            nodeList[toRemove - 1].next = nodeList[toRemove].next;
+            return head;
+
+            // First solution: 2 while-loops, less memory, longer runtime.
+            //if (head == null)
+            //    return head;
+            //
+            //ListNode node = head;
+            //int length = 1;
+            //while(node.next != null)
+            //{
+            //    length++;
+            //    node = node.next;   
+            //}
+            //
+            //if (length == 1)
+            //    return null;
+            //
+            //int toRemove = length - n;
+            //if(toRemove == 0)
+            //{
+            //    head = head.next;
+            //    return head;
+            //}
+            //
+            //int current = 1;
+            //node = head;
+            //while(current < toRemove)
+            //{
+            //    node = node.next;
+            //    current++;
+            //}
+            //
+            //node.next = node.next.next;
+            //
+            //return head;
+        }
+
+        /*
+            Remove the Nth node from the list and return the head.
+            Constraints:
+                The number of nodes in the list is sz.
+                1 <= sz <= 30
+                0 <= Node.val <= 100
+                1 <= n <= sz
+         */
+    }
+}
